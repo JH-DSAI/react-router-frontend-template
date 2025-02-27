4111e791215a7bdf65f24c9a3ecd35360e76b2a1
iain maitland <github@iainmaitland.com>
2025-02-27 14:09:11 -0500

    b612

--- app/root.jsx
+++ app/root.jsx
@@ -20,7 +20,7 @@ export const links = () => [
   },
   {
     rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
+    href: 'https://fonts.googleapis.com/css2?family=B612+Mono:ital,wght@0,400;0,700;1,400;1,700&family=B612:ital,wght@0,400;0,700;1,400;1,700&display=swap',
   },
   { rel: 'stylesheet', href: toastStyles },
   { rel: 'stylesheet', href: stylesheet },
